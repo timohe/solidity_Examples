afd825e85e4fd6284dba1f98b2c42d4675c60f14
Timo Hegnauer <t.hegnauer@gmail.com>
2018-05-04 12:10:16 +0200

    Balance of Contract Example

---
+++ balanceOfContract.sol
@@ -0,0 +1,13 @@
+pragma solidity ^0.4.21;
+
+contract Payout
+{
+   function() public payable {}
+
+    function getBalance() public constant returns(uint bal) {
+        bal = address(this).balance;
+    }
+    function payout() public {
+        msg.sender.transfer(2);
+    }
+}
